c713b3acac47910c8c84a974dd9ec4b7ad4ac4fe
Joe Mizzi <themizzi@me.com>
2020-10-12 20:49:54 -0500

    initial commit

---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+
+$filesPath = __DIR__.'/files';
+if (file_exists($filesPath)) {
+    $files = glob($filesPath . '/*');
+    foreach ($files as $index => $file) {
+        $message = sprintf("removing %s\n", $index);
+        echo $message;
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+    rmdir($filesPath);
+}
+
+if (!mkdir($filesPath) && !is_dir($filesPath)) {
+    throw new \RuntimeException(sprintf('Directory "%s" was not created', 'files'));
+}
+
+$max = 1;
+
+foreach(range(1, $max) as $index) {
+    $message = sprintf("creating %s\n", $index);
+    echo $message;
+    file_put_contents(__DIR__ . '/files/file' . $index, '');
+}
+
+do {
+    $message = sprintf("trying file count %s\n", $max);
+    echo $message;
+    $iterator = new \RecursiveDirectoryIterator($filesPath);
+    $array1 = iterator_to_array($iterator);
+    $array2 = scandir($filesPath);
+    ++$max;
+    file_put_contents(__DIR__ . '/files/file' . $max, '');
+} while (count($array1) === count($array2));
+
+$message = sprintf("found files %s vs %s\n", count($array1), count($array2));
+echo $message;
